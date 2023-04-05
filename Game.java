bd597a4abdfacbd3e9c8d3662e732f8a89ee01eb
galenhan02 <76551836+galenhan02@users.noreply.github.com>
2023-04-04 22:22:56 -0700

    added new lines to make experience smoother

--- Game.java
+++ Game.java
@@ -98,6 +98,9 @@ public class Game {
     }
 
     public void render() {
+        for(int i = 0; i < height; i++) {
+            System.out.println("");
+        }
         for(int i = -1; i < board.length + 1; i++) {
             for(int j = -1; j < board[0].length + 1; j++) {
                if(i == -1 || i == board.length) {
